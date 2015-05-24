84c35e08f35a55d9f6bea8e1cccee706c29645c9
AlejoNext <alejonext@gmail.com>
2015-05-23 23:58:58 -0500

    Is a object

--- social-share.android.js
+++ social-share.android.js
@@ -2,40 +2,52 @@ var application = require("application");
 var context = application.android.context;
 var numberOfImagesCreated = 0;
 
-module.exports = {
-	shareImage: function(image, subject) {
-		numberOfImagesCreated ++;
-		subject = subject || "How would you like to share this image?";
+/*
+var social = require('social-share');
+var share = new social('My Title');
+share.Text('My Text is Cool');
 
-		var intent = new android.content.Intent(android.content.Intent.ACTION_SEND);
-		intent.setType("image/jpeg");
+OR
+var share = require('social-share');
+share.Text('Hello!!');
+share.Image('~/My.jpg');
+*/
 
-		var stream = new java.io.ByteArrayOutputStream();
-		image.android.compress(android.graphics.Bitmap.CompressFormat.JPEG, 100, stream);
-		var path = android.provider.MediaStore.Images.Media.insertImage(
-			context.getContentResolver(), image.android, "TempFile" + numberOfImagesCreated, null);
-		intent.putExtra(android.content.Intent.EXTRA_STREAM,
-			android.net.Uri.parse(path));
+function share (name) {
+	this.name = name;
+	this.init();
+}
 
-		var shareIntent = android.content.Intent.createChooser(intent, subject);
-		shareIntent.setFlags(android.content.Intent.FLAG_ACTIVITY_NEW_TASK);
-		context.startActivity(shareIntent);
-	},
-	/*
-		social.shareText('My tweet!', 'Now');
-	*/
-	shareText : function (subject, text, name) {
-		// Convert a String
-		var name = name || "How would you like to share?";
-		var subject = subject + '';
-		var text = text + '';
-
-		var intent = new android.content.Intent(android.content.Intent.ACTION_SEND);
-		intent.setType("text/plain");
-		intent.putExtra(android.content.Intent.EXTRA_SUBJECT, subject);
-		intent.putExtra(android.content.Intent.EXTRA_TEXT, text);
-
-		var shareIntent = android.content.Intent.createChooser(intent, name);
-		context.startActivity(shareIntent);
-	}
+share.prototype.init = function() {
+	this.intent = new android.content.Intent(android.content.Intent.ACTION_SEND);
 };
+
+share.prototype.__send = function(intent) {
+	context.startActivity(android.content.Intent.createChooser(intent, this.name || 'Share'));
+};
+
+share.prototype.Image = function(image, subject) {
+	this.init();
+	this.intent.setType("image/jpeg");
+	numberOfImagesCreated ++;
+	var stream = new java.io.ByteArrayOutputStream();
+	image.android.compress(android.graphics.Bitmap.CompressFormat.JPEG, 100, stream);
+	var path = android.provider.MediaStore.Images.Media.insertImage(
+		context.getContentResolver(), image.android, "TempFile" + numberOfImagesCreated, null);
+	this.intent.putExtra(android.content.Intent.EXTRA_STREAM, android.net.Uri.parse(path));
+	if(typeof subject === 'string')
+		this.intent.putExtra(android.content.Intent.EXTRA_SUBJECT, subject);
+	this.__send(this.intent);
+};
+
+share.prototype.Text = function(subject, text) {
+	this.init();
+	this.intent.setType("text/plain");
+	if(typeof subject === 'string')
+		this.intent.putExtra(android.content.Intent.EXTRA_SUBJECT, subject);
+	if(typeof text === 'string')
+		this.intent.putExtra(android.content.Intent.EXTRA_TEXT, text);
+	this.__send(this.intent);
+};
+
+module.exports = share;
