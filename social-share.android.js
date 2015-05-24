344eeb8dad2a59481bd2224679e688440ed00ae0
AlejoNext <alejonext@gmail.com>
2015-05-23 23:20:45 -0500

    Share text

--- social-share.android.js
+++ social-share.android.js
@@ -20,5 +20,22 @@ module.exports = {
 		var shareIntent = android.content.Intent.createChooser(intent, subject);
 		shareIntent.setFlags(android.content.Intent.FLAG_ACTIVITY_NEW_TASK);
 		context.startActivity(shareIntent);
+	},
+	/*
+		social.shareText('My tweet!', 'Now');
+	*/
+	shareText : function (subject, text, name) {
+		// Convert a String
+		var name = name || "How would you like to share?";
+		var subject = subject + '';
+		var text = text + '';
+
+		var intent = new android.content.Intent(android.content.Intent.ACTION_SEND);
+		intent.setType("text/plain");
+		intent.putExtra(android.content.Intent.EXTRA_SUBJECT, subject);
+		intent.putExtra(android.content.Intent.EXTRA_TEXT, text);
+
+		var shareIntent = android.content.Intent.createChooser(intent, name);
+		context.startActivity(shareIntent);
 	}
 };
