08953544d40099badf3c095d36670eb3de0cd1e3
AlejoNext <alejonext@gmail.com>
2015-05-24 00:27:03 -0500

    Multiple run

--- social-share.android.js
+++ social-share.android.js
@@ -1,55 +1,55 @@
-var application = require("application");
-var context = application.android.context;
+var application = require('application');
 var numberOfImagesCreated = 0;
 
 /*
 var social = require('social-share');
 var share = new social('My Title');
 share.Text('My Text is Cool');
+share.Image('~/My.jpg');
 
 OR
+
 var share = require('social-share');
 share.Text('Hello!!');
 share('My name').Image('~/My.jpg');
 */
 
+function sends (intent, name) {
+	application.android.context.startActivity(android.content.Intent.createChooser(intent, name || 'Share'));
+}
+
 function share (name) {
 	this.name = name;
-	this.init();
 }
 
 share.prototype.init = function() {
-	if(!this.intent)
-		this.intent = new android.content.Intent(android.content.Intent.ACTION_SEND);
-};
-
-share.prototype.__send = function(intent) {
-	context.startActivity(android.content.Intent.createChooser(intent, this.name || 'Share'));
+	this.intent = new android.content.Intent(android.content.Intent.ACTION_SEND);
 };
 
 share.prototype.Image = function(image, subject) {
 	this.init();
-	this.intent.setType("image/jpeg");
-	numberOfImagesCreated ++;
+	this.intent.setType('image/jpeg');
+	numberOfImagesCreated++;
 	var stream = new java.io.ByteArrayOutputStream();
 	image.android.compress(android.graphics.Bitmap.CompressFormat.JPEG, 100, stream);
 	var path = android.provider.MediaStore.Images.Media.insertImage(
-		context.getContentResolver(), image.android, "TempFile" + numberOfImagesCreated, null);
+		context.getContentResolver(), image.android, 'TempFile' + numberOfImagesCreated, null);
 	this.intent.putExtra(android.content.Intent.EXTRA_STREAM, android.net.Uri.parse(path));
 	if(typeof subject === 'string')
 		this.intent.putExtra(android.content.Intent.EXTRA_SUBJECT, subject);
-	this.__send(this.intent);
+	sends(this.intent, this.name);
 };
 
 share.prototype.Url =
 share.prototype.Text = function (subject, text) {
 	this.init();
-	this.intent.setType("text/plain");
+	this.intent.setType('text/plain');
 	if(typeof subject === 'string')
 		this.intent.putExtra(android.content.Intent.EXTRA_SUBJECT, subject);
 	if(typeof text === 'string')
 		this.intent.putExtra(android.content.Intent.EXTRA_TEXT, text);
-	this.__send(this.intent);
+
+	sends(this.intent, this.name);
 };
 
 module.exports = share;
