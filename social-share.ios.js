c5816965913f2b01155b0eb2556becfe9cdd0e75
TJ VanToll <tj.vantoll@gmail.com>
2015-10-12 11:52:35 -0400

    Adding iPad support

--- social-share.ios.js
+++ social-share.ios.js
@@ -1,6 +1,18 @@
-function share(thingToShare) {
+var frameModule = require("ui/frame");
+
+function share(thingToShare, index) {
 	var activityController = UIActivityViewController.alloc()
 		.initWithActivityItemsApplicationActivities([thingToShare], null);
+	var presentViewController = activityController.popoverPresentationController;
+	if (presentViewController) {
+		var page = frameModule.topmost().currentPage;
+		if (page && page.ios.navigationItem.rightBarButtonItems &&
+			page.ios.navigationItem.rightBarButtonItems.count > 0) {
+			presentViewController.barButtonItem = page.ios.navigationItem.rightBarButtonItems[0];
+		} else {
+			presentViewController.sourceView = page.ios.view;
+		}
+	}
 	UIApplication.sharedApplication().keyWindow.rootViewController
 		.presentViewControllerAnimatedCompletion(activityController, true, null);
 }
