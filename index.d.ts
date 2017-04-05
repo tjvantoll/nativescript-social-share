98d8e72dbd3b06db1d5ef35fb5bc7cd79e9539f1
Esa Väntsi <esa.vantsi@vincit.fi>
2017-04-05 13:35:14 +0300

    Update index.d

--- index.d.ts
+++ index.d.ts
@@ -2,7 +2,7 @@
 
     /**
      * Share an image.
-     * @param {ImageSource} - The image to share.
+     * @param {ImageSource} image - The image to share.
      * @param {string} [subject] - The subject of the share *** ANDROID ONLY ***
      */
     export function shareImage(image?: ImageSource, subject?: string);
@@ -10,7 +10,16 @@
 
     /**
     * Share text.
-    * @param {ImageSource} - The image to share.
+    * @param {string} text - Text to share.
     * @param {string} [subject] - The subject of the share *** ANDROID ONLY ***
     */
     export function shareText(text: string, subject?: string);
+
+
+    /**
+     * Share URL.
+     * @param {string} url - URL to share.
+     * @param {string} text - Text to share with URL.
+     * @param {string} [subject] - The subject of the share *** ANDROID ONLY ***
+     */
+    export function shareUrl(url: string, text: string, subject?: string);
